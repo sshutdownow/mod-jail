a07b856db799932198be95cd8aad7d094dc1753c
iPopovi <iPopovi@dac9c9cf-0e1d-0410-9445-c7a8077285c1>
2007-08-02 11:56:56 +0000

    svn id

--- trunk/mod_jail.c
+++ trunk/mod_jail.c
@@ -42,7 +42,7 @@
 
 #define JAIL_CTX "global::jail_module_ctx"
 
-static const char __unused cvsid[] = "$svn:author$";
+static const char __unused cvsid[] = "$CVSID$";
 
 typedef struct {
     struct jail jail;
