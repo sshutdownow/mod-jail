91fc860f6c157d41440617160252ae7925499a9a
iPopovi <iPopovi@dac9c9cf-0e1d-0410-9445-c7a8077285c1>
2009-12-14 23:52:06 +0000

    Added merged patch for Apache 2.2.14 that contains both jail and chroot

--- patch-os:unix:unixd.c
+++ patch-os:unix:unixd.c
@@ -1,64 +1,66 @@
 --- os/unix/unixd.c.orig	2008-09-18 21:42:18.000000000 +0300
-+++ os/unix/unixd.c	2009-08-30 12:07:08.000000000 +0300
-@@ -118,20 +118,20 @@
++++ os/unix/unixd.c	2009-12-15 01:12:31.000000000 +0200
+@@ -118,6 +118,36 @@
          return -1;
      }
  
--    if (NULL != unixd_config.chroot_dir) {
++#if defined(JAIL_API_VERSION)
 +    if (NULL != unixd_config.jail.path) {
-         if (geteuid()) {
-             ap_log_error(APLOG_MARK, APLOG_ALERT, errno, NULL,
--                         "Cannot chroot when not started as root");
++        if (geteuid()) {
++            ap_log_error(APLOG_MARK, APLOG_ALERT, errno, NULL,
 +                         "Cannot jail when not started as root");
-             return -1;
-         }
--        if (chdir(unixd_config.chroot_dir) != 0) {
++            return -1;
++        }
 +        if (chdir(unixd_config.jail.path) != 0) {
-             ap_log_error(APLOG_MARK, APLOG_ALERT, errno, NULL,
--                         "Can't chdir to %s", unixd_config.chroot_dir);
++            ap_log_error(APLOG_MARK, APLOG_ALERT, errno, NULL,
 +                         "Can't chdir to %s", unixd_config.jail.path);
-             return -1;
-         }
--        if (chroot(unixd_config.chroot_dir) != 0) {
++            return -1;
++        }
 +        if (jail(&unixd_config.jail) == -1) {
-             ap_log_error(APLOG_MARK, APLOG_ALERT, errno, NULL,
--                         "Can't chroot to %s", unixd_config.chroot_dir);
++            ap_log_error(APLOG_MARK, APLOG_ALERT, errno, NULL,
 +                         "Can't jail to %s", unixd_config.jail.path);
-             return -1;
-         }
-         if (chdir("/") != 0) {
-@@ -139,6 +139,13 @@
-                          "Can't chdir to new root");
-             return -1;
-         }
++            return -1;
++        }
++        if (chdir("/") != 0) {
++            ap_log_error(APLOG_MARK, APLOG_ALERT, errno, NULL,
++                         "Can't chdir to new root");
++            return -1;
++        }
 +        if (unixd_config.jail_securelevel > 0) {
 +    	    if (sysctl((int[]){ CTL_KERN, KERN_SECURELVL }, 2, 0, 0,
 +    		    &unixd_config.jail_securelevel, sizeof(unixd_config.jail_securelevel)) != 0)
 +    		ap_log_error(APLOG_MARK, APLOG_ALERT, errno, NULL,
 +    		             "Can't set kern.securelevel via sysctl()");
-+    		                         
 +        }
++    }
++#else /* chroot */
+     if (NULL != unixd_config.chroot_dir) {
+         if (geteuid()) {
+             ap_log_error(APLOG_MARK, APLOG_ALERT, errno, NULL,
+@@ -140,6 +170,7 @@
+             return -1;
+         }
      }
++#endif /* JAIL_API_VERSION */
  
  #ifdef MPE
-@@ -222,7 +229,7 @@
+     /* Only try to switch if we're running as MANAGER.SYS */
+@@ -222,6 +253,63 @@
  
      return NULL;
  }
--AP_DECLARE(const char *) unixd_set_chroot_dir(cmd_parms *cmd, void *dummy,
++#if defined(JAIL_API_VERSION)
 +AP_DECLARE(const char *) unixd_set_jail_dir(cmd_parms *cmd, void *dummy,
-                                               const char *arg)
- {
-     const char *err = ap_check_cmd_context(cmd, GLOBAL_ONLY);
-@@ -230,10 +237,51 @@
-         return err;
-     }
-     if (!ap_is_directory(cmd->pool, arg)) {
--        return "ChrootDir must be a valid directory";
++                                              const char *arg)
++{
++    const char *err = ap_check_cmd_context(cmd, GLOBAL_ONLY);
++    if (err != NULL) {
++        return err;
++    }
++    if (!ap_is_directory(cmd->pool, arg)) {
 +        return "JailDir must be a valid directory";
-     }
- 
--    unixd_config.chroot_dir = arg;
++    }
++
 +    unixd_config.jail.path = arg;
 +    return NULL;
 +}
@@ -85,9 +87,9 @@
 +	return "could not make sense of jail ip address";
 +    }
 +
-+#if defined(JAIL_API_VERSION) && (JAIL_API_VERSION == 2)
++#if JAIL_API_VERSION == 2
 +    unixd_config.jail.ip4[0].s_addr = in.s_addr;
-+#else
++#else /* JAIL_API_VERSION == 0 */
 +    unixd_config.jail.ip_number = ntohl(in.s_addr);
 +#endif
 +    return NULL;
@@ -101,15 +103,28 @@
 +    }
 +
 +    unixd_config.jail_securelevel = strtol(arg, 0, 10) & 0x03;
++    return NULL;
++}
++#else /* chroot */
+ AP_DECLARE(const char *) unixd_set_chroot_dir(cmd_parms *cmd, void *dummy,
+                                               const char *arg)
+ {
+@@ -236,7 +323,7 @@
+     unixd_config.chroot_dir = arg;
      return NULL;
  }
- 
-@@ -245,7 +293,20 @@
+-
++#endif /* JAIL_API_VERSION */
+ AP_DECLARE(void) unixd_pre_config(apr_pool_t *ptemp)
+ {
+     apr_finfo_t wrapper;
+@@ -245,8 +332,24 @@
      unixd_config.user_id = ap_uname2id(DEFAULT_USER);
      unixd_config.group_id = ap_gname2id(DEFAULT_GROUP);
      
--    unixd_config.chroot_dir = NULL; /* none */
-+#if defined(JAIL_API_VERSION) && (JAIL_API_VERSION == 2)
++#if defined(JAIL_API_VERSION)
++    unixd_config.jail_securelevel = 3;
++#if JAIL_API_VERSION == 2
 +    unixd_config.jail.version = JAIL_API_VERSION;
 +    unixd_config.jail.path = NULL; /* none */
 +    unixd_config.jail.hostname = "localhost";
@@ -119,10 +134,13 @@
 +    unixd_config.jail.ip4 = apr_pcalloc(ptemp, sizeof(struct in_addr));
 +    unixd_config.jail.ip4[0].s_addr = htonl(INADDR_LOOPBACK);
 +    unixd_config.jail.ip6 = NULL;
-+#else
++#else /* JAIL_API_VERSION == 0 */
 +    unixd_config.jail = (struct jail) { .version = 0, .path = NULL, .hostname = "localhost", .ip_number = INADDR_LOOPBACK };
-+#endif
-+    unixd_config.jail_securelevel = 3;
- 
++#endif /* JAIL_API_VERSION == XX */
++#else /* chroot */
+     unixd_config.chroot_dir = NULL; /* none */
+-
++#endif /* JAIL_API_VERSION */
      /* Check for suexec */
      unixd_config.suexec_enabled = 0;
+     if ((apr_stat(&wrapper, SUEXEC_BIN,
--- patch-os:unix:unixd.h
+++ patch-os:unix:unixd.h
@@ -1,17 +1,14 @@
 --- os/unix/unixd.h.orig	2008-09-18 21:42:18.000000000 +0300
-+++ os/unix/unixd.h	2009-05-06 22:49:26.000000000 +0300
-@@ -48,6 +48,21 @@
++++ os/unix/unixd.h	2009-12-15 01:15:16.000000000 +0200
+@@ -48,6 +48,27 @@
  #include <sys/ipc.h>
  #endif
  
-+#ifndef __FreeBSD__
-+#error "This patch for FreeBSD only!!!"
-+#endif
-+
++#if defined(__FreeBSD__)
 +#include <osreldate.h>
-+#if  !defined(__FreeBSD_version) || (__FreeBSD_version < 400000)
-+#error "The jail() system call appeared in FreeBSD 4.0"
-+#endif
++
++#if defined(__FreeBSD_version) && (__FreeBSD_version >= 400000)
++/* Jail(2) patch for FreeBSD */
 +
 +#include <sys/param.h>
 +#include <sys/jail.h>
@@ -19,24 +16,36 @@
 +#include <netinet/in.h>
 +#include <arpa/inet.h>
 +
++#ifndef JAIL_API_VERSION
++#define JAIL_API_VERSION 0
++#endif
++
++#endif /* __FreeBSD_version */
++
++#endif /* __FreeBSD__ */
++
++
  typedef struct {
      uid_t uid;
      gid_t gid;
-@@ -72,7 +87,8 @@
+@@ -72,7 +93,12 @@
      uid_t user_id;
      gid_t group_id;
      int suexec_enabled;
--    const char *chroot_dir;
++#if defined(JAIL_API_VERSION)
 +    struct jail jail;
 +    int jail_securelevel;
++#else /* chroot */
+     const char *chroot_dir;
++#endif
  } unixd_config_rec;
  AP_DECLARE_DATA extern unixd_config_rec unixd_config;
  
-@@ -82,7 +98,13 @@
+@@ -82,8 +108,19 @@
                                          const char *arg);
  AP_DECLARE(const char *) unixd_set_group(cmd_parms *cmd, void *dummy, 
                                           const char *arg);
--AP_DECLARE(const char *) unixd_set_chroot_dir(cmd_parms *cmd, void *dummy, 
++#if defined(JAIL_API_VERSION)
 +AP_DECLARE(const char *) unixd_set_jail_dir(cmd_parms *cmd, void *dummy, 
 +                                              const char *arg);
 +AP_DECLARE(const char *) unixd_set_jail_hostname(cmd_parms *cmd, void *dummy, 
@@ -44,15 +53,25 @@
 +AP_DECLARE(const char *) unixd_set_jail_address(cmd_parms *cmd, void *dummy, 
 +                                              const char *arg);
 +AP_DECLARE(const char *) unixd_set_jail_securelevel(cmd_parms *cmd, void *dummy, 
++                                              const char *arg);
++#else /* chroot */
+ AP_DECLARE(const char *) unixd_set_chroot_dir(cmd_parms *cmd, void *dummy, 
                                                const char *arg);
++#endif /* JAIL_API_VERSION */
  					 
  #if defined(RLIMIT_CPU) || defined(RLIMIT_DATA) || defined(RLIMIT_VMEM) || defined(RLIMIT_NPROC) || defined(RLIMIT_AS)
-@@ -116,8 +138,15 @@
-   "Effective user id for this server"), \
- AP_INIT_TAKE1("Group", unixd_set_group, NULL, RSRC_CONF, \
-   "Effective group id for this server"), \
--AP_INIT_TAKE1("ChrootDir", unixd_set_chroot_dir, NULL, RSRC_CONF, \
--    "The directory to chroot(2) into")
+ AP_DECLARE(void) unixd_set_rlimit(cmd_parms *cmd, struct rlimit **plimit,
+@@ -111,6 +148,24 @@
+ #define ap_os_killpg(x, y)      (kill (-(x), (y)))
+ #endif /* HAVE_KILLPG */
+ 
++#if defined(JAIL_API_VERSION)
++
++#define UNIX_DAEMON_COMMANDS	\
++AP_INIT_TAKE1("User", unixd_set_user, NULL, RSRC_CONF, \
++  "Effective user id for this server"), \
++AP_INIT_TAKE1("Group", unixd_set_group, NULL, RSRC_CONF, \
++  "Effective group id for this server"), \
 +AP_INIT_TAKE1("JailDir", unixd_set_jail_dir, NULL, RSRC_CONF, \
 +    "The directory to jail(2) into"), \
 +AP_INIT_TAKE1("JailHostname", unixd_set_jail_hostname, NULL, RSRC_CONF, \
@@ -62,6 +81,16 @@
 +AP_INIT_TAKE1("JailSecureLevel", unixd_set_jail_securelevel, NULL, RSRC_CONF, \
 +    "The securelevel inside jail prison")
 +
++#else /* chroot */
++
+ #define UNIX_DAEMON_COMMANDS	\
+ AP_INIT_TAKE1("User", unixd_set_user, NULL, RSRC_CONF, \
+   "Effective user id for this server"), \
+@@ -119,5 +174,7 @@
+ AP_INIT_TAKE1("ChrootDir", unixd_set_chroot_dir, NULL, RSRC_CONF, \
+     "The directory to chroot(2) into")
  
++#endif /* JAIL_API_VERSION */
++
  #endif
  /** @} */
